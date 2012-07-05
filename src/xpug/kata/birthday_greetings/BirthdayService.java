ceca723d35d82f0d4bd7b15b06a7e144ef43a3e6
Blake Boesinger <blake@boesinger.org>
2012-07-05 19:18:53 +0100

    format code

--- src/xpug/kata/birthday_greetings/BirthdayService.java
+++ src/xpug/kata/birthday_greetings/BirthdayService.java
@@ -19,16 +19,16 @@ public class BirthdayService {
     public static final String SUBJECT = "Happy Birthday!";
 
 
-   class Email {
+    class Email {
 
-       public final String recipient;
-       public final String body;
+        public final String recipient;
+        public final String body;
 
-       Email(String recipient, String body) {
-           this.recipient = recipient;
-           this.body = body;
-       }
-   }
+        Email(String recipient, String body) {
+            this.recipient = recipient;
+            this.body = body;
+        }
+    }
 
     public void sendGreetings(String fileName, OurDate ourDate, String smtpHost, int smtpPort) throws IOException, ParseException, AddressException, MessagingException {
 
@@ -36,64 +36,59 @@ public class BirthdayService {
         List<Employee> employeesWithBirthdayToday = new ArrayList<Employee>();
 
 
-		BufferedReader in = new BufferedReader(new FileReader(fileName));
-		String str = "";
-		str = in.readLine(); // skip header
-		while ((str = in.readLine()) != null) {
-			String[] employeeData = str.split(", ");
-			Employee employee = new Employee(employeeData[1], employeeData[0], employeeData[2], employeeData[3]);
-			if (employee.isBirthday(ourDate)) {
-
+        BufferedReader in = new BufferedReader(new FileReader(fileName));
+        String str = "";
+        str = in.readLine(); // skip header
+        while ((str = in.readLine()) != null) {
+            String[] employeeData = str.split(", ");
+            Employee employee = new Employee(employeeData[1], employeeData[0], employeeData[2], employeeData[3]);
+            if (employee.isBirthday(ourDate)) {
                 employeesWithBirthdayToday.add(employee);
-
-
-			}
-		}
+            }
+        }
 
 
         for (Employee employee : employeesWithBirthdayToday) {
             String recipient = employee.getEmail();
-            				String body = "Happy Birthday, dear %NAME%!".replace("%NAME%", employee.getFirstName());
-
-                            Email email = new Email(recipient, body);
-                            toSend.add(email);
-
+            String body = "Happy Birthday, dear %NAME%!".replace("%NAME%", employee.getFirstName());
+            Email email = new Email(recipient, body);
+            toSend.add(email);
         }
 
-        for ( Email email : toSend) {
-        sendMessage(smtpHost, smtpPort, "sender@here.com", SUBJECT, email.body, email.recipient);
+        for (Email email : toSend) {
+            sendMessage(smtpHost, smtpPort, "sender@here.com", SUBJECT, email.body, email.recipient);
         }
-	}
+    }
 
-	private void sendMessage(String smtpHost, int smtpPort, String sender, String subject, String body, String recipient) throws AddressException, MessagingException {
-		// Create a mail session
-		java.util.Properties props = new java.util.Properties();
-		props.put("mail.smtp.host", smtpHost);
-		props.put("mail.smtp.port", "" + smtpPort);
-		Session session = Session.getDefaultInstance(props, null);
+    private void sendMessage(String smtpHost, int smtpPort, String sender, String subject, String body, String recipient) throws AddressException, MessagingException {
+        // Create a mail session
+        java.util.Properties props = new java.util.Properties();
+        props.put("mail.smtp.host", smtpHost);
+        props.put("mail.smtp.port", "" + smtpPort);
+        Session session = Session.getDefaultInstance(props, null);
 
-		// Construct the message
-		Message msg = new MimeMessage(session);
-		msg.setFrom(new InternetAddress(sender));
-		msg.setRecipient(Message.RecipientType.TO, new InternetAddress(recipient));
-		msg.setSubject(subject);
-		msg.setText(body);
+        // Construct the message
+        Message msg = new MimeMessage(session);
+        msg.setFrom(new InternetAddress(sender));
+        msg.setRecipient(Message.RecipientType.TO, new InternetAddress(recipient));
+        msg.setSubject(subject);
+        msg.setText(body);
 
-		// Send the message
-		sendMessage(msg);
-	}
+        // Send the message
+        sendMessage(msg);
+    }
 
-	// made protected for testing :-(
-	protected void sendMessage(Message msg) throws MessagingException {
-		Transport.send(msg);
-	}
+    // made protected for testing :-(
+    protected void sendMessage(Message msg) throws MessagingException {
+        Transport.send(msg);
+    }
 
-	public static void main(String[] args) {
-		BirthdayService service = new BirthdayService();
-		try {
-			service.sendGreetings("employee_data.txt", new OurDate("2008/10/08"), "localhost", 25);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+    public static void main(String[] args) {
+        BirthdayService service = new BirthdayService();
+        try {
+            service.sendGreetings("employee_data.txt", new OurDate("2008/10/08"), "localhost", 25);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
